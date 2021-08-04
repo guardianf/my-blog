8af3a2d9241eefa1c6ea5f0c5d540565ff70574a
Fu Yubin <578248377@qq.com>
2021-08-04 10:03:33 +0800

    split home to a devided file

--- src/App.vue
+++ src/App.vue
@@ -9,77 +9,29 @@
         <li>找我</li>
       </ul>
     </div>
-    <div class="page first-page" ref="home">
-      <p>我叫傅裕彬,</p>
-      <p>一只90年出生,</p>
-      <p>不务正业的前端工程🦁️</p>
-      <p class='flex-row'>
-        <a>{{ animateText }}</a>
-        <a class="splitor"></a>
-      </p>
-    </div>
+    <home ref='home' />
     <!-- <div class="page" style="background-color: blue"></div>
     <div class="page" style="background-color: green"></div> -->
   </div>
 </template>
 
 <script>
+import Home from '@/pages/home';
+
 export default {
   name: 'App',
-  data() {
-    return {
-      like: '喜欢',
-      something: ''
-    }
-  },
-  created() {
-    this.getAnimateText();
-  },
-  computed: {
-    animateText() {
-      return `${this.like}${this.something}`;
-    },
-  },
+  components: { Home },
   methods: {
-    getAnimateText() {
-      const texts = ["設計。", "美食。", "各種亂七八糟的東西。"];
-      let index = 0;// display count
-      const inter = 200;
-      let itlTextTime = inter * (texts[index % texts.length].length + 2);
-      const itlTextFunc = () => {
-        let text = texts[index % texts.length];
-        let len = 2 * (text.length + 1);
-        let i = 0;
-        const itlFunc = () => {
-          if(i <= len) {
-            const str = text.slice(0, i <= (len / 2) ? i : len - i);
-            this.something = str;
-            console.log(str,i);
-          } else {
-            clearInterval(itl);
-            index++;
-            itlTextFunc();
-          }
-          i++;
-        };
-        itlFunc();
-        const itl = setInterval(itlFunc, inter);
-      }
-      itlTextFunc();
-    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.flex-row {
-  display: flex;
-  align-items: center;
-}
+@import "~@/styles/main.scss";
 
 #app {
   .banner {
-    @extend .flex-row;
+    @extend .flex-row !optional;
     height: 70px;
     box-shadow: rgba($color: #000000, $alpha: 0.2) 0px 0px 3px 2px;
     padding: 0 20px;
@@ -90,42 +42,12 @@ export default {
     right: 0;
     background-color: #fff;
     .row {
-      @extend .flex-row;
+      @extend .flex-row !optional;
       & > * {
         padding: 0 10px;
         line-height: 20px;
       }
     }
-  }
-
-  .page {
-    width: 100%;
-    height: 100vh;
-  }
-}
-.first-page {
-  @extend .flex-row;
-  justify-content: center;
-  flex-flow: column;
-  background-image: url(./assets/images/download.svg);
-  p {
-    font-size: 40px;
-    color: #fff;
-  }
-  .splitor {
-    background-color: #000;
-    width: 3px;
-    height: 80%;
-    visibility: visible;
-    animation: twinkle 2s infinite;
-  }
-}
-@keyframes twinkle {
-  0% {
-    opacity:.0.8;
-  }
-  100%{
-    opacity:0;
   }
 }
 </style>
---
+++ src/pages/home.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="page first-page" ref="home">
+    <p>我叫傅裕彬,</p>
+    <p>一只90年出生,</p>
+    <p>不务正业的前端工程🦁️</p>
+    <p class='flex-row'>
+      <a>{{ animateText }}</a>
+      <a class="splitor"></a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      like: '喜欢',
+      something: ''
+    }
+  },
+  created() {
+    this.getAnimateText();
+  },
+  computed: {
+    animateText() {
+      return `${this.like}${this.something}`;
+    },
+  },
+  methods: {
+    getAnimateText() {
+      const texts = ["設計。", "美食。", "各種亂七八糟的東西。"];
+      let index = 0;// display count
+      const inter = 200;
+      let itlTextTime = inter * (texts[index % texts.length].length + 2);
+      const itlTextFunc = () => {
+        let text = texts[index % texts.length];
+        let len = 2 * (text.length + 1);
+        let i = 0;
+        const itlFunc = () => {
+          if(i <= len) {
+            const str = text.slice(0, i <= (len / 2) ? i : len - i);
+            this.something = str;
+            console.log(str,i);
+          } else {
+            clearInterval(itl);
+            index++;
+            itlTextFunc();
+          }
+          i++;
+        };
+        itlFunc();
+        const itl = setInterval(itlFunc, inter);
+      }
+      itlTextFunc();
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/styles/main.scss";
+
+@keyframes twinkle {
+  0% {
+    opacity:.0.8;
+  }
+  100%{
+    opacity:0;
+  }
+}
+
+.first-page {
+  @extend .flex-row !optional;
+  justify-content: center;
+  flex-flow: column;
+  background-image: url(~@/assets/images/download.svg);
+  p {
+    font-size: 40px;
+    color: #fff;
+  }
+  .splitor {
+    background-color: #000;
+    width: 4px;
+    margin-left: 4px;
+    height: 60%;
+    visibility: visible;
+    animation: twinkle 2s infinite;
+  }
+}
+</style>
